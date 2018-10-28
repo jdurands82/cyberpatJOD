bf7bb35d179a0ee2071714c5652e3491f9003138
jdurands82 <44537597+jdurands82@users.noreply.github.com>
2018-10-27 22:07:56 -0400

    Add files via upload

---
+++ badfeaturefinder.ps1
@@ -0,0 +1,5 @@
+﻿<#Deletes bad services from bad.txt on the desktop CHANGE FILEPATH #>
+$bad = @(
+    get-content C:\users\PCDC\Desktop\bad.txt
+)
+Get-WindowsOptionalFeature -online | where-object{$bad -contains $_.featurename} 
---
+++ removeadmins.ps1
@@ -0,0 +1,23 @@
+﻿<#deletes bad admins from group
+needs goodadmins.txt from desktop in which Administrator be included#>
+$goodAdmins = get-content C:\Users\PCDC\Desktop\goodadmins.txt | foreach-object {"$env:computername\$_"}
+Echo "Found GoodAdmins"
+$currentAdmins = get-localgroupmember -group "Administrators"
+Echo "Found current Admins"
+$invalidAdmins = $currentAdmins | where-object{$goodAdmins -notcontains $_.name}
+if($invalidAdmins.Length -gt 0)
+{
+    Echo "Invalid admins found: `r`n$invalidadmins"
+    $choice = read-host "Remove?"
+    if($choice.tolower() -eq "y")
+        {
+        $invalidAdmins| Remove-LocalGroupMember -group "Administrators"
+        echo "Removed `r`n$invalidAdmins"
+        }
+    }
+    else
+    {
+    echo "All Valid"
+ }
+
+
