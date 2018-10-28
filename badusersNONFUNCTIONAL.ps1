f11f3e669db8114b160384f918a28745ad4ec1ea
jdurands82 <44537597+jdurands82@users.noreply.github.com>
2018-10-28 09:31:05 -0400

    Update badusersNONFUNCTIONAL.ps1

--- badusersNONFUNCTIONAL.ps1
+++ badusersNONFUNCTIONAL.ps1
@@ -3,7 +3,7 @@ needs goodusers.txt from desktop in which Administrator be included#>
 $goodusers = get-content C:\Users\PCDC\Desktop\goodusers.txt | foreach-object {"$env:computername\$_"}
 $currentusers = get-localgroupmember -group "Users"
 $invalidUsers = $currentUsers | where-object{$goodUsers -notcontains $_.name}
-if($invalidAdmins.Length -gt 0)
+if($invalidusers.Length -gt 0)
 {
     Echo "Invalid Users found: `r`n$invalidUsers"
     $choice = read-host "Remove?"
