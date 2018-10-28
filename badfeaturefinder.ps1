c274baf71fd4e89ff00e5dfbe4d9de661eeee589
jdurands82 <44537597+jdurands82@users.noreply.github.com>
2018-10-27 22:10:58 -0400

    Update badfeaturefinder.ps1

--- badfeaturefinder.ps1
+++ badfeaturefinder.ps1
@@ -1,5 +1,6 @@
-﻿<#Deletes bad services from bad.txt on the desktop CHANGE FILEPATH #>
+﻿<#Finds bad services from bad.txt on the desktop, CHANGE FILEPATH #>
 $bad = @(
     get-content C:\users\PCDC\Desktop\bad.txt
 )
-Get-WindowsOptionalFeature -online | where-object{$bad -contains $_.featurename} 
+Get-WindowsOptionalFeature -online | where-object{$bad -contains $_.featurename}
+
