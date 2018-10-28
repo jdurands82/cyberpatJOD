ab19052fb40f3ab4a2b4b20661325dbad36b2d3b
jdurands82 <44537597+jdurands82@users.noreply.github.com>
2018-10-27 22:28:37 -0400

    Update badusers.ps1

--- badusers.ps1
+++ badusers.ps1
@@ -1,1 +1,19 @@
-
+<#deletes bad admins from group
+needs goodadmins.txt from desktop in which Administrator be included#>
+$goodusers = get-content C:\Users\PCDC\Desktop\goodusers.txt | foreach-object {"$env:computername\$_"}
+$currentusers = get-localgroupmember -group "Users"
+$invalidUsers = $currentUsers | where-object{$goodUsers -notcontains $_.name}
+if($invalidAdmins.Length -gt 0)
+{
+    Echo "Invalid Users found: `r`n$invalidUsers"
+    $choice = read-host "Remove?"
+    if($choice.tolower() -eq "y")
+        {
+        $invalidUsers| Remove-LocalGroupMember -group "Users"
+        echo "Removed `r`n$invalidUsers"
+        }
+    }
+    else
+    {
+    echo "All Valid"
+ }
